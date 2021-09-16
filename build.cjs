ccef64e4074c6b472035ccf8a3dc46844e4d3bd0
Logan McAnsh <logan@mcan.sh>
2021-09-15 21:17:49 -0500

    feat: build using estrella and add typedefs
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ build.cjs
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+let { build, ts, tsconfig, dirname, glob, log } = require("estrella");
+
+let baseConfig = {
+  entryPoints: ["src/index.ts"],
+  outdir: "dist",
+};
+
+build({
+  ...baseConfig,
+  format: "esm",
+  sourcemap: true,
+  outExtension: { ".js": ".mjs" },
+  onEnd(config) {
+    generateTypeDefs(config);
+  },
+});
+
+build({
+  ...baseConfig,
+  format: "cjs",
+  sourcemap: true,
+  outExtension: { ".js": ".cjs" },
+  onEnd(config) {
+    generateTypeDefs(config);
+  },
+});
+
+build({
+  ...baseConfig,
+  format: "esm",
+  sourcemap: "inline",
+  outExtension: { ".js": ".debug.mjs" },
+  onEnd(config) {
+    generateTypeDefs(config);
+  },
+});
+
+build({
+  ...baseConfig,
+  format: "cjs",
+  sourcemap: "inline",
+  outExtension: { ".js": ".debug.cjs" },
+  onEnd(config) {
+    generateTypeDefs(config);
+  },
+});
+
+function generateTypeDefs(config) {
+  let tsc = tsconfig(config);
+
+  let tscInclude = tsc.include ? tsc.include.map((i) => glob(i)) : [];
+  let entryPoints = Array.isArray(config.entry) ? config.entry : [config.entry];
+
+  let filenames = Array.from(new Set(...tscInclude, ...entryPoints));
+
+  log.info("Generating type declaration files for", filenames.join(", "));
+
+  let compilerOptions = {
+    ...tsconfig.compilerOptions,
+    moduleResolution: undefined,
+    declaration: true,
+    outDir: config.outdir,
+  };
+
+  let program = ts.ts.createProgram(filenames, compilerOptions);
+  let targetSourceFile = undefined;
+  let writeFile = undefined;
+  let cancellationToken = undefined;
+  let emitOnlyDtsFiles = true;
+
+  program.emit(
+    targetSourceFile,
+    writeFile,
+    cancellationToken,
+    emitOnlyDtsFiles
+  );
+
+  log.info("Wrote", glob(config.outdir + "/*.d.ts").join(", "));
+}
