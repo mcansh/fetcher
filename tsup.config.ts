000b58333ec7efa0e4041adb063519d6754b8b99
Logan McAnsh <logan@mcan.sh>
2023-07-18 09:08:23 -0400

    chore: bundle with tsup
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+import type { Options } from "tsup";
+
+export default defineConfig(() => {
+  let options: Options = {
+    entry: ["src/index.ts"],
+    sourcemap: true,
+    external: [],
+    tsconfig: "./tsconfig.json",
+    dts: true,
+  };
+
+  return [
+    {
+      ...options,
+      format: "cjs",
+    },
+
+    {
+      ...options,
+      format: "esm",
+    },
+  ];
+});
